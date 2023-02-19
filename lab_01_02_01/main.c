bb81079ea176318886aeda44f8323f6856bebc05
kkd22u221 <kkd22u221@student.bmstu.ru>
2023-02-19 20:33:37 +0300

    Style corrections

--- lab_01_02_01/main.c
+++ lab_01_02_01/main.c
@@ -1,39 +1,43 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #define PI 3.14159265358979323846
-
 
 int main()
 {
     double a, b, angle_val, s, c, side;
 
     printf("Enter value of the first base of the trapezoid: ");
-    if (scanf("%lf", &a) != 1 || a <= 0) {
+    if (scanf("%lf", &a) != 1 || a <= 0)
+    {
         fprintf(stderr, "Input error. Value should be bigger than zero.");
         return 1;
     }
 
     printf("Enter value of the second base of the trapezoid: ");
-    if (scanf("%lf", &b) != 1 || b <= 0) {
+    if (scanf("%lf", &b) != 1 || b <= 0)
+    {
         fprintf(stderr, "Input error. Value should be bigger than zero.");
         return 1;
     }
 
     printf("Enter value of the angle: ");
-    if (scanf("%lf", &angle_val) != 1 || angle_val <= 0 || angle_val >= 90) {
+    if (scanf("%lf", &angle_val) != 1 || angle_val <= 0 || angle_val >= 90)
+    {
         fprintf(stderr, "Input error. Value should be bigger than zero.");
         return 1;
     }
 
-    if (a > b) {
+    if (a > b)
+    {
         c = a;
         a = b;
         b = c;
     }
 
     side = (b - a) / 2.0 / cos(angle_val * PI / 180.0);
-    if (a + side + side <= b || b + side + side <= a || a + b + side <= side) {
+    if (a + side + side <= b || b + side + side <= a || a + b + side <= side)
+    {
         printf("The trapezoid with this parameters does not exist.");
         return 1;
     }
