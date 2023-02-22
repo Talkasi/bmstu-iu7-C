3c47199380d5d1edc31a45cbafcac57622e8b3c8
Talkasi <kitalkasi@gmail.com>
2023-02-22 18:56:29 +0300

    Tests and code for lab_01_09_01 added

---
+++ lab_01_09_01/main.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    double x;
+    double g = 0.0;
+    double n = 1.0;
+
+    printf("Enter first number: ");
+    if (scanf("%lf", &x) != 1 || x < 0) {
+        printf("Input error.\n");
+        return 1;
+    }
+
+    while (x >= 0)
+    {
+        g += sqrt(x / n);
+        ++n;
+        printf("Enter next number: ");
+        if (scanf("%lf", &x) != 1) {
+            printf("Input error.\n");
+            return 1;
+        }
+    }
+
+    g = sin(g);
+
+    printf("%lf", g);
+}
