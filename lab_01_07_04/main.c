a25127968347d527279f2c2e2730d29325233c4e
Talkasi <kitalkasi@gmail.com>
2023-02-26 15:32:56 +0300

    Lab_01_07_04 updated

--- lab_01_07_04/main.c
+++ lab_01_07_04/main.c
@@ -1,46 +1,52 @@
 #include <math.h>
 #include <stdio.h>
 
-int main()
+double s_computation(double x, double eps);
+
+int main(void)
 {
     double x, eps;
-    double eps_float = 1e-8;
-    double f, s, elem;
+    double f, s;
     double absolute_error, relative_error;
-    long long s_parameter;
 
     printf("Enter x: ");
-    if (scanf("%lf", &x) != 1 || fabs(x) >= 1)
+    if (scanf("%lf", &x) != 1 || fabs(x) >= 1.0)
     {
         printf("Input error");
         return 1;
     }
 
     printf("Enter eps: ");
-    if (scanf("%lf", &eps) != 1 || eps > 1 || eps <= 0)
+    if (scanf("%lf", &eps) != 1 || eps > 1.0 || eps <= 0.0)
     {
         printf("Input error.");
         return 1;
     }
 
-    f = 1.0 / (1 + x) / (1 + x) / (1 + x);
-
-    s = 1.0;
-    s_parameter = 2.0;
-    elem = s;
-
-    while (fabs(elem) - eps > eps_float)
-    {
-        ++s_parameter;
-        elem = -elem * s_parameter / (s_parameter - 2.0) * x;
-        s += elem;
-    }
+    f = 1.0 / (1.0 + x) / (1.0 + x) / (1.0 + x);
+    s = s_computation(x, eps);
 
     absolute_error = fabs(f - s);
     relative_error = fabs(f - s) / fabs(f);
 
-    printf("S(x): %lf\nF(x): %lf\n", s, f);
-    printf("Absolute error: %lf\nRelative error: %lf\n", absolute_error, relative_error);
+    printf("S(x): %.6lf\nF(x): %.6lf\n", s, f);
+    printf("Absolute error: %.6lf\nRelative error: %.6lf\n", absolute_error, relative_error);
 
     return 0;
+}
+
+double s_computation(double x, double eps)
+{
+    double elem = 1.0;
+    double s = 1.0;
+    int i = 0;
+
+    while (fabs(elem) > eps)
+    {
+        ++i;
+        elem *= -(i + 2) * x / i;
+        s += elem;
+    }
+
+    return s;
 }
