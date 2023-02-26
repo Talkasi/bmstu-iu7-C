c8b71cf83291437d7222a2ef3f9d96094ae00c9e
Talkasi <kitalkasi@gmail.com>
2023-02-26 15:56:05 +0300

    Lab_01_09_01 updated

--- lab_01_09_01/main.c
+++ lab_01_09_01/main.c
@@ -1,24 +1,23 @@
 #include <math.h>
 #include <stdio.h>
 
-int main()
+int main(void)
 {
     double x;
     double g = 0.0;
     double n = 1.0;
 
     printf("Enter first number: ");
-    if (scanf("%lf", &x) != 1 || x < 0)
+    if (scanf("%lf", &x) != 1 || x < 0.0)
     {
         printf("Input error.\n");
         return 1;
     }
 
-    while (x >= 0)
+    while (x >= 0.0)
     {
         g += sqrt(x / n);
         ++n;
-        printf("Enter next number: ");
         if (scanf("%lf", &x) != 1)
         {
             printf("Input error.\n");
@@ -28,5 +27,5 @@ int main()
 
     g = sin(g);
 
-    printf("%lf", g);
+    printf("%.6lf", g);
 }
