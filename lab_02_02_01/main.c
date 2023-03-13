04ad11e22eb50ffaf6726d779b7e259f450e968a
Talkasi <kitalkasi@gmail.com>
2023-03-13 16:42:03 +0300

    Lab02_02 style corrections

--- lab_02_02_01/main.c
+++ lab_02_02_01/main.c
@@ -3,51 +3,44 @@
 
 #define NMAX 10
 
-int is_prime(long number);
+int is_prime(int number);
+int read_array(int *arr, int arr_len);
+int prime_copy(int *dst, int *src, int src_len);
 
 int main(void)
 {
-    long arr[NMAX], cp_arr[NMAX];
-    int len_arr, len_cp_arr = 0;
-    int i;
+    int arr[NMAX], cp_arr[NMAX];
+    int arr_len, cp_arr_len;
 
     printf("Enter number of elements in the array: ");
-    if (scanf("%d", &len_arr) != 1 || len_arr <= 0 || len_arr > 10)
+    if (scanf("%d", &arr_len) != 1 || arr_len <= 0 || arr_len > 10)
     {
         printf("Input error.");
         return 1;
     }
 
-    for (i = 0; i < len_arr; ++i)
+    if (read_array(arr, arr_len))
     {
-        printf("Enter element of an array: ");
-        if (scanf("%ld", &arr[i]) != 1)
-        {
-            printf("Input error.");
-            return 1;
-        }
+        printf("Input error.");
+        return 1;
     }
 
-    for (i = 0; i < len_arr; ++i)
-        if (is_prime(arr[i]))
-            cp_arr[len_cp_arr++] = arr[i];
+    cp_arr_len = prime_copy(cp_arr, arr, arr_len);
 
-    if (len_cp_arr > 0)
-    {
-        printf("Elements of the new array:");
-        for (int i = 0; i < len_cp_arr; ++i)
-            printf(" %ld", cp_arr[i]);
-
-        return 0;
-    }
-    else
+    if (cp_arr_len <= 0)
     {
         printf("There aren't prime numbers in the given array.\n");
         return 1;
     }
+
+    printf("Elements of the new array:");
+    for (int i = 0; i < cp_arr_len; ++i)
+        printf(" %d", cp_arr[i]);
+
+    return 0;
 }
 
-int is_prime(long number)
+int is_prime(int number)
 {
     if (number <= 0 || number == 1)
         return 0;
@@ -55,11 +48,34 @@ int is_prime(long number)
     if (number == 2)
         return 1;
 
-    for (long i = 2; i < sqrt(number) + 1; ++i)
+    for (int i = 2; i < sqrt(number) + 1; ++i)
     {
         if (number % i == 0)
             return 0;
     }
 
     return 1;
+}
+
+int read_array(int *arr, int arr_len)
+{
+    for (int i = 0; i < arr_len; ++i)
+    {
+        printf("Enter element of an array: ");
+        if (scanf("%d", &arr[i]) != 1)
+            return 1;
+    }
+
+    return 0;
+}
+
+int prime_copy(int *dst, int *src, int src_len)
+{
+    int dst_len = 0;
+
+    for (int i = 0; i < src_len; ++i)
+        if (is_prime(src[i]))
+            dst[dst_len++] = src[i];
+
+    return dst_len;
 }
