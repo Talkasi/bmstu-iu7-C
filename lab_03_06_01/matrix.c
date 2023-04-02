61288e30a0c03af14c94d7a2e72a0fadeadc016e
Talkasi <kitalkasi@gmail.com>
2023-04-02 14:00:56 +0300

    Lab03_06 added

---
+++ lab_03_06_01/matrix.c
@@ -0,0 +1,44 @@
+#include "matrix.h"
+#include <stdio.h>
+
+void matrix_print(arr_t matrix[N_COLUMNS_MAX], size_t n_rows, size_t n_columns)
+{
+    for (size_t i = 0; i < n_rows; ++i)
+    {
+        for (size_t j = 0; j < n_columns; ++j)
+            printf("%8d ", matrix[i][j]);
+
+        printf("\n");
+    }
+}
+
+void spiral_init(arr_t matrix[N_COLUMNS_MAX], size_t n_rows, size_t n_columns)
+{
+    size_t i_start = 0, i_end = 0;
+    size_t j_start = 0, j_end = 0;
+    size_t i = 0;
+    size_t j = 0;
+    int number = 1;
+
+    while (number <= (int)(n_rows * n_columns))
+    {
+        matrix[i][j] = number++;
+
+        if (i == i_start && j < n_columns - j_end - 1)
+            ++j;
+        else if (j == n_columns - j_end - 1 && i < n_rows - i_end - 1)
+            ++i;
+        else if (i == n_rows - i_end - 1 && j > j_end)
+            --j;
+        else
+            --i;
+
+        if ((i == i_start + 1) && (j == j_start) && (j_start != n_columns - j_end - 1))
+        {
+            ++i_start;
+            ++i_end;
+            ++j_start;
+            ++j_end;
+        }
+    }
+}
---
+++ lab_03_06_01/matrix.h
@@ -0,0 +1,13 @@
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+#include <stdlib.h>
+
+#define N_COLUMNS_MAX 10
+#define N_ROWS_MAX 10
+
+typedef int arr_t[N_ROWS_MAX];
+
+void matrix_print(arr_t matrix[N_COLUMNS_MAX], size_t n_rows, size_t n_columns);
+void spiral_init(arr_t matrix[N_COLUMNS_MAX], size_t n_rows, size_t n_columns);
+
+#endif
