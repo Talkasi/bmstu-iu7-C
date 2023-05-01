901b9d8034bdf9df946b9c292c597d0eba4d6e1e
kkd22u221 <kkd22u221@student.bmstu.ru>
2023-05-01 12:10:35 +0300

    Lab04_01 corrections

--- lab_04_01/main.c
+++ lab_04_01/main.c
@@ -2,27 +2,40 @@
 #include <stdio.h>
 #include <string.h>
 
-#define N_TESTS 8
-
 int main(void)
 {
     int n_errors = 0;
 
-    char *s[N_TESTS] = { "", " ", "", "char", "charchar", "char", "1222224", "123" };
-    char *param[N_TESTS] = { "", "", "char", "char", "char", "?", "229", "32" };
-    char c[N_TESTS] = { '\0', ' ', 'b', 'a', 'r', 'c', '9', '1' };
-
     {
+        char *s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+        char *temp_s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+
+        char *param[] = { "", "", "char", "c", "h", "?", "229", "32", "q" };
+        char *temp_param[] = { "", "", "char", "c", "h", "?", "229", "32", "q" };
+
         /* my_strpbrk testing */
-        for (size_t i = 0; i < N_TESTS; ++i)
+        for (size_t i = 0; i < sizeof(s) / sizeof(char *); ++i)
         {
             char *my_res = my_strpbrk(s[i], param[i]);
             char *res = strpbrk(s[i], param[i]);
 
-            if (my_res != res)
+            size_t n_s = 0;
+            size_t n_param = 0;
+
+            for (size_t j = 0; j < sizeof(s) / sizeof(char *); ++j)
+            {
+                if (s[j] != temp_s[j])
+                    ++n_s;
+
+                if (param[j] != temp_param[j])
+                    ++n_param;
+            }
+
+            if (my_res != res || n_s || n_param)
             {
                 printf(">> Error in my_strpbrk\nInput parameters: %20s %20s\n", s[i], param[i]);
                 printf("Submitted:        %20s\nExpected:         %20s\n", my_res, res);
+                printf("`s` parameter was changed %zu times\n`param` parameter was changed %zu times\n", n_s, n_param);
 
                 ++n_errors;
             }
@@ -30,16 +43,35 @@ int main(void)
     }
 
     {
+        char *s[] = { "", " ", "", "char", "charchar", "char", "1222224", "222224" };
+        char *temp_s[] = { "", " ", "", "char", "charchar", "char", "1222224", "222224" };
+
+        char *param[] = { "", "", "char", "char", "char", "?", "229", "32" };
+        char *temp_param[] = { "", "", "char", "char", "char", "?", "229", "32" };
+
         /* my_strspn testing */
-        for (size_t i = 0; i < N_TESTS; ++i)
+        for (size_t i = 0; i < sizeof(s) / sizeof(char *); ++i)
         {
             size_t my_res = my_strspn(s[i], param[i]);
             size_t res = strspn(s[i], param[i]);
 
-            if (my_res != res)
+            size_t n_s = 0;
+            size_t n_param = 0;
+
+            for (size_t j = 0; j < sizeof(s) / sizeof(char *); ++j)
+            {
+                if (s[j] != temp_s[j])
+                    ++n_s;
+
+                if (param[j] != temp_param[j])
+                    ++n_param;
+            }
+
+            if (my_res != res || n_s || n_param)
             {
                 printf(">> Error in my_strspn\nInput parameters: %20s %20s\n", s[i], param[i]);
                 printf("Submitted:        %20zu\nExpected:         %20zu\n", my_res, res);
+                printf("`s` parameter was changed %zu times\n`param` parameter was changed %zu times\n", n_s, n_param);
 
                 ++n_errors;
             }
@@ -47,16 +79,35 @@ int main(void)
     }
 
     {
+        char *s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+        char *temp_s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+
+        char *param[] = { "", "", "char", "gc", "char", "?", "229", "32", "hjrl" };
+        char *temp_param[] = { "", "", "char", "gc", "char", "?", "229", "32", "hjrl" };
+
         /* my_strcspn testing */
-        for (size_t i = 0; i < N_TESTS; ++i)
+        for (size_t i = 0; i < sizeof(s) / sizeof(char *); ++i)
         {
             size_t my_res = my_strcspn(s[i], param[i]);
             size_t res = strcspn(s[i], param[i]);
 
-            if (my_res != res)
+            size_t n_s = 0;
+            size_t n_param = 0;
+
+            for (size_t j = 0; j < sizeof(s) / sizeof(char *); ++j)
+            {
+                if (s[j] != temp_s[j])
+                    ++n_s;
+
+                if (param[j] != temp_param[j])
+                    ++n_param;
+            }
+
+            if (my_res != res || n_s || n_param)
             {
                 printf(">> Error in my_strcspn\nInput parameters: %20s %20s\n", s[i], param[i]);
                 printf("Submitted:        %20zu\nExpected:         %20zu\n", my_res, res);
+                printf("`s` parameter was changed %zu times\n`param` parameter was changed %zu times\n", n_s, n_param);
 
                 ++n_errors;
             }
@@ -64,16 +115,35 @@ int main(void)
     }
 
     {
+        char *s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+        char *temp_s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+
+        char c[] = { '\0', ' ', 'b', 'a', 'r', 'c', '2', '7', 'q' };
+        char temp_c[] = { '\0', ' ', 'b', 'a', 'r', 'c', '2', '7', 'q' };
+
         /* my_strchr testing */
-        for (size_t i = 0; i < N_TESTS; ++i)
+        for (size_t i = 0; i < sizeof(s) / sizeof(char *); ++i)
         {
             char *my_res = my_strchr(s[i], c[i]);
             char *res = strchr(s[i], c[i]);
 
-            if (my_res != res)
+            size_t n_s = 0;
+            size_t n_c = 0;
+
+            for (size_t j = 0; j < sizeof(s) / sizeof(char *); ++j)
+            {
+                if (s[j] != temp_s[j])
+                    ++n_s;
+
+                if (c[j] != temp_c[j])
+                    ++n_c;
+            }
+
+            if (my_res != res || n_s || n_c)
             {
                 printf(">> Error in my_strchr\nInput parameters: %20s %20c\n", s[i], c[i]);
                 printf("Submitted:        %20s\nExpected:         %20s\n", my_res, res);
+                printf("`s` parameter was changed %zu times\n`c` parameter was changed %zu times\n", n_s, n_c);
 
                 ++n_errors;
             }
@@ -81,16 +151,35 @@ int main(void)
     }
 
     {
+        char *s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+        char *temp_s[] = { "", " ", "", "char", "charchar", "char", "1222224", "123", "quqareque" };
+
+        char c[] = { '\0', ' ', 'b', 'a', 'r', 'c', '2', '7', 'q' };
+        char temp_c[] = { '\0', ' ', 'b', 'a', 'r', 'c', '2', '7', 'q' };
+
         /* my_strrchr testing */
-        for (size_t i = 0; i < N_TESTS; ++i)
+        for (size_t i = 0; i < sizeof(s) / sizeof(char *); ++i)
         {
             char *my_res = my_strrchr(s[i], c[i]);
             char *res = strrchr(s[i], c[i]);
 
-            if (my_res != res)
+            size_t n_s = 0;
+            size_t n_c = 0;
+
+            for (size_t j = 0; j < sizeof(s) / sizeof(char *); ++j)
             {
-                printf(">> Error in my_strrchr\nInput parameters: %20s %20c\n", s[i], c[i]);
+                if (s[j] != temp_s[j])
+                    ++n_s;
+
+                if (c[j] != temp_c[j])
+                    ++n_c;
+            }
+
+            if (my_res != res || n_s || n_c)
+            {
+                printf(">> Error in my_strchr\nInput parameters: %20s %20c\n", s[i], c[i]);
                 printf("Submitted:        %20s\nExpected:         %20s\n", my_res, res);
+                printf("`s` parameter was changed %zu times\n`c` parameter was changed %zu times\n", n_s, n_c);
 
                 ++n_errors;
             }
