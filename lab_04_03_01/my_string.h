b59c4cf58ceec87b1e0d1f89b0631d1e0cf72c78
Talkasi <kitalkasi@gmail.com>
2023-05-09 19:48:33 +0300

    Lab04_corrections

--- lab_04_03_01/my_string.h
+++ lab_04_03_01/my_string.h
@@ -3,8 +3,9 @@
 #include <stddef.h>
 
 #define MAX_WORD_LEN 17
+#define MAX_STR_LEN 257
 
-size_t line_scan(char *s, size_t s_max_len);
+size_t line_scan(char *s);
 
 size_t line_special_create(char *s, char words[][MAX_WORD_LEN], size_t n_words);
 size_t arr_words_fill(char words[][MAX_WORD_LEN], char *s);
--- lab_04_04_01/my_string.c
+++ lab_04_04_01/my_string.c
@@ -1,22 +1,31 @@
 #include "my_string.h"
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int digits_skip(char *s, size_t *cur_i);
 
-int line_scan(char *s, size_t s_max_len)
+size_t line_scan(char *s)
 {
-    int c;
-    size_t i = 0;
+    char temp_s[MAX_STR_LEN + 1];
+    char *temp_p = fgets(temp_s, MAX_STR_LEN + 1, stdin);
 
-    while ((c = getchar()) != '\n' && c != EOF)
-        if (i < s_max_len - 1)
-            s[i++] = c;
-        else
-            return s_max_len + 1;
+    if (temp_p == NULL || temp_s[0] == '\n')
+    {
+        s[0] = '\0';
+        return 0;
+    }
 
-    s[i] = '\0';
-    return i;
+    size_t len_temp_s = strlen(temp_s);
+    if (len_temp_s == MAX_STR_LEN && temp_s[len_temp_s - 1] != '\n')
+        return MAX_STR_LEN + 1;
+
+    if (temp_s[len_temp_s - 1] == '\n')
+        temp_s[--len_temp_s] = '\0';
+
+    strcpy(s, temp_s);
+
+    return len_temp_s;
 }
 
 int is_real(char *s)
