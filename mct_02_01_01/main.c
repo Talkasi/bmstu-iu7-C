05ff20a7e7ffd542c7cef3c2be95e9d2cc16b157
kkd22u221 <kkd22u221@student.bmstu.ru>
2023-05-29 19:10:38 +0300

    Mct02 corrections

--- mct_02_01_01/main.c
+++ mct_02_01_01/main.c
@@ -1,35 +1,109 @@
-#include <stdio.h>
 #include "my_file.h"
+#include <stdio.h>
+#include <string.h>
 
-int print_all_students(FILE *f);
-int add_student(FILE *f, struct student *new_student);
+#define WRONG_PARAMETERS 53
+#define OPEN_FILE_ERROR 1
+#define FILE_SIZE_ERROR 2
+#define WRONG_STUDENT_DATA 3
 
-int main(int argc, char *argv[]) {
-	if (argc < 2)
-	{
-		printf("Error. No file to work with.\n");
-		return 0;
-	}
-	
- 	FILE *f;
- 	fopen(f, argv[1]);
- 	
-	fclose(f);
- 	
- 	return 1;
+size_t line_scan(char *s);
+int read_student(struct student *new_student);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        return WRONG_PARAMETERS;
+
+    if (strcmp(argv[1], "p") == 0)
+    {
+        FILE *f = fopen(argv[2], "rb");
+        if (f == NULL)
+            return OPEN_FILE_ERROR;
+
+        long fsize = file_size(f);
+        if (fsize == 0 || fsize % sizeof(struct student) != 0)
+        {
+            fclose(f);
+            return FILE_SIZE_ERROR;
+        }
+
+        print_all_students(f);
+        fclose(f);
+    }
+    else if (strcmp(argv[1], "a") == 0)
+    {
+        FILE *f = fopen(argv[2], "rb+");
+        if (f == NULL)
+            return OPEN_FILE_ERROR;
+
+        long fsize = file_size(f);
+        if (fsize % sizeof(struct student) != 0)
+        {
+            fclose(f);
+            return FILE_SIZE_ERROR;
+        }
+
+        struct student new_student = { "", 0 };
+        int rc = read_student(&new_student);
+        if (rc)
+        {
+            fclose(f);
+            return rc;
+        }
+
+        add_student(f, &new_student);
+        fclose(f);
+    }
+    else if (strcmp(argv[1], "d") == 0)
+    {
+        FILE *f = fopen(argv[2], "rb+");
+        if (f == NULL)
+            return OPEN_FILE_ERROR;
+
+        long fsize = file_size(f);
+        if (fsize == 0 || fsize % sizeof(struct student) != 0)
+        {
+            fclose(f);
+            return FILE_SIZE_ERROR;
+        }
+
+        double avrg;
+        avrg_height(f, &avrg);
+        delete_students_by_cond(f, avrg);
+        fclose(f);
+    }
+    else
+        return WRONG_PARAMETERS;
+
+    return 0;
 }
 
-int print_all_students(FILE *f) {
-	struct student cur_student;
-	while (fread(&cur_student, sizeof(struct student), 1, f) == 1) {
-		printf("%5s: %d\n", cur_student.last_name, cur_student.heigh);
-	}
-	
-	return ferror(f);
+size_t line_scan(char *s)
+{
+    char temp_s[MAX_NAME_LEN + 1];
+    if (fgets(temp_s, MAX_NAME_LEN + 1, stdin) == NULL)
+        return 0;
+
+    size_t len_temp_s = strlen(temp_s);
+    if ((len_temp_s == MAX_NAME_LEN + 1 && temp_s[len_temp_s - 1] != '\n'))
+        return MAX_NAME_LEN + 1;
+
+    if (temp_s[len_temp_s - 1] == '\n')
+        temp_s[--len_temp_s] = '\0';
+
+    strcpy(s, temp_s);
+    return len_temp_s;
 }
 
-int add_student(FILE *f, struct student *new_student) {
-	fwrite(new_student, sizeof(student), 1, f);
-	
-	return ferror(f);
+int read_student(struct student *new_student)
+{
+    int rc;
+    if ((rc = line_scan(new_student->last_name)) == 0 || rc == MAX_NAME_LEN + 1)
+        return WRONG_STUDENT_DATA;
+
+    if (scanf("%" SCNu32, &new_student->height) != 1 || new_student->height == 0)
+        return WRONG_STUDENT_DATA;
+
+    return 0;
 }
--- mct_02_01_01/my_file.c
+++ mct_02_01_01/my_file.c
@@ -1,31 +1,68 @@
 #include "my_file.h"
 
-int average(FILE *f, int *average) {
-	struct student cur_student;
-	*average = 0;
-	size_t i = 0;
-	while (fread(&cur_student, sizeof(struct student), 1, f) == 1) {
-		*average += cur_student.heigh;
-		++i;
-	}
-	*average /= (int)i;
-	return ferror(f);
+int print_all_students(FILE *f)
+{
+    fseek(f, 0, SEEK_SET);
+    struct student cur_student;
+
+    do
+    {
+        if (fread(&cur_student, sizeof(struct student), 1, f) != 1)
+            return FILE_READ_ERROR;
+        printf("%s: %d\n", cur_student.last_name, cur_student.height);
+    } while (!feof(f));
+
+    return 0;
 }
 
-int delete_students_by_cond(FILE *f, int average) {
-	struct student cur_student;
-	size_t i = 0;
-	long cur_pos;
-	while (fread(&cur_student, sizeof(struct student), 1, f) == 1) {
-		if (cur_student.heigh >= average) {
-			cur_pos = ftell(f);
-			fseek(f, -i * sizeof(student), SEEK_CUR);
-			fwrite(cur_student, sizeof(student), 1, f);
-			fseek(f, cur_pos, SEEK_SET);
-		}
-		else
-			++i;
-	}
-	
-	return ferror(f);
+int avrg_height(FILE *f, double *average)
+{
+    fseek(f, 0, SEEK_SET);
+    struct student cur_student;
+    *average = 0;
+    size_t i = 0;
+    while (fread(&cur_student, sizeof(struct student), 1, f) == 1)
+    {
+        *average += cur_student.height;
+        ++i;
+    }
+
+    *average /= (double)i;
+    return feof(f) ? 0 : -1;
+}
+
+int delete_students_by_cond(FILE *f, double average)
+{
+    fseek(f, 0, SEEK_SET);
+    struct student cur_student;
+    size_t n = 0;
+    long cur_pos;
+
+    while (fread(&cur_student, sizeof(struct student), 1, f) == 1)
+        if (cur_student.height >= (uint32_t)average)
+        {
+            cur_pos = ftell(f);
+            fseek(f, n * sizeof(struct student), SEEK_SET);
+            fwrite(&cur_student, sizeof(struct student), 1, f);
+            fseek(f, cur_pos, SEEK_SET);
+            ++n;
+        }
+
+    return feof(f) ? 0 : -1;
+}
+
+long file_size(FILE *f)
+{
+    long cur_pos = ftell(f);
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, cur_pos, SEEK_SET);
+    return fsize;
+}
+
+int add_student(FILE *f, struct student *new_student)
+{
+    fseek(f, 0, SEEK_END);
+    fwrite(new_student, sizeof(struct student), 1, f);
+    return ferror(f);
 }
